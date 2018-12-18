e532ce7d790e16f93c6ed7c382f561eb194c05e9
Dalya Samur <dsamur@sogyo.nl>
2018-12-18 16:31:55 +0100

    Python code for moving the robot

--- robot_move.py
+++ robot_move.py
@@ -11,38 +11,45 @@ BP.set_sensor_type(BP.PORT_1,
                    BP.SENSOR_TYPE.TOUCH)  # Configure for a touch sensor. If an EV3 touch sensor is connected, it will be configured for EV3 touch, otherwise it'll configured for NXT touch.
 
 try:
-    # print("Press touch sensor on port 1 to run motors")
+    print("Press touch sensor on port 1 to run motors")
     value = 0
     while not value:
         try:
+            print("First loop: before get_sensor ")
             value = BP.get_sensor(BP.PORT_1)
+            print("First loop: after get_sensor ")
         except brickpi3.SensorError:
             pass
 
     speed = 30
     adder = 1
     while True:
+        print("Second loop: before get_sensor ")
         # BP.get_sensor retrieves a sensor value.
         # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
         # BP.get_sensor returns the sensor value.
         # try:
         value = BP.get_sensor(BP.PORT_1)
+        print("Second loop: after get_sensor ")
         # except brickpi3.SensorError as error:
         #     print(error)
         #     value = 0
 
         if value:  # if the touch sensor is pressed
             speed = 0
+            print("Third - if then: set speed 0 ")
             # if speed <= -100 or speed >= 100:  # if speed reached 100, start ramping down. If speed reached -100, start ramping up.
             #     adder = -adder
             # speed += adder
         else:  # else the touch sensor is not pressed or not configured, so set the speed to 0
             speed = 30
+            print("Third - if then: set speed 30 ")
             # speed = 0
             # adder = 1
 
         # Set the motor speed for all four motors
         BP.set_motor_power(BP.PORT_A + BP.PORT_D, speed)
+        print("MOTOR POWER ON")
 
         # try:
         #     # Each of the following BP.get_motor_encoder functions returns the encoder value (what we want to display).
